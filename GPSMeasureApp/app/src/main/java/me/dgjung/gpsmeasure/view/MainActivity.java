09437f21fd7f53bd75c78e3bd8b1ca9aa8a81563
DONGGOO JUNG <dgjung0220.gmail.com>
2020-07-02 09:34:39 +0900

    week2 finished

--- GPSMeasureApp/app/src/main/java/me/dgjung/gpsmeasure/view/MainActivity.java
+++ GPSMeasureApp/app/src/main/java/me/dgjung/gpsmeasure/view/MainActivity.java
@@ -1,30 +1,47 @@
 package me.dgjung.gpsmeasure.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
+import me.dgjung.gpsmeasure.BuildConfig;
 import me.dgjung.gpsmeasure.R;
 import me.dgjung.gpsmeasure.adapter.MeasureAdapter;
 import me.dgjung.gpsmeasure.db.DatabaseContract;
 import me.dgjung.gpsmeasure.db.DatabaseHelper;
+import me.dgjung.gpsmeasure.service.Utils;
 import me.dgjung.gpsmeasure.vo.Measurements;
+
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
+import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+import static me.dgjung.gpsmeasure.service.Utils.getCurrentTimes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter measureAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private TextView hardwareName, hardwareYear;
 
     Button btnMeasureStart;
     private List<Measurements> measurementsList;
@@ -42,42 +60,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Permission  check
+        ActivityCompat.requestPermissions(this, Utils.permissionUtils.REQUIRED_PERMISSIONS, Utils.permissionUtils.PERMISSION_REQUEST_CODE);
+
+
         dbhelper = new DatabaseHelper(this);
         btnMeasureStart = findViewById(R.id.measure_start_btn);
         db = dbhelper.getReadableDatabase();
         mRecyclerView = findViewById(R.id.main_recyclerview);
+        hardwareName = findViewById(R.id.hardware_name_value);
+        hardwareYear = findViewById(R.id.hardware_year_value);
 
         btnMeasureStart.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // add
-                // RANDOM DATA
-                Random rnd = new Random();
-
-                ContentValues values = new ContentValues();
-                values.put(DatabaseContract.MeasurementsDatabase.COL_NAME_1, rnd.nextInt(5000) + 200);
-                values.put(DatabaseContract.MeasurementsDatabase.COL_NAME_2, rnd.nextInt(500) + 200);
-                values.put(DatabaseContract.MeasurementsDatabase.COL_NAME_3, getCurrentTimes());
-
-                values.put(DatabaseContract.MeasurementsDatabase.COL_NAME_4, "location_file_path.csv");
-                values.put(DatabaseContract.MeasurementsDatabase.COL_NAME_5, "measure_file_path.csv");
-                values.put(DatabaseContract.MeasurementsDatabase.COL_NAME_6, "navigation_file_path.csv");
-                values.put(DatabaseContract.MeasurementsDatabase.COL_NAME_7, "gpsStatus_file_path.csv");
-                values.put(DatabaseContract.MeasurementsDatabase.COL_NAME_8, "nmea_file_path.csv");
-
-                long rowId = db.insert(DatabaseContract.MeasurementsDatabase.TABLE_NAME, null, values);
-                if (rowId != -1) {
-                    Toast.makeText(MainActivity.this, "Add successfully", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
-                }
-
-                Intent intent = getIntent();
-                finish();
+                Intent intent = new Intent(MainActivity.this, SensorActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
+
+        Intent intent = getIntent();
+        ArrayList<File> file_list = (ArrayList<File>) intent.getSerializableExtra("FILES");
+
+        if (file_list != null ){
+            Intent emailIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+            emailIntent.setType("*/*");
+            //emailIntent.putExtra(Intent.EXTRA_SUBJECT, "SensorLog");
+            //emailIntent.putExtra(Intent.EXTRA_TEXT, "");
+
+            ArrayList<Parcelable> urls = new ArrayList<>();
+
+            for(File mFile : file_list) {
+
+                Uri fileURI =
+                        FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID, mFile);
+                urls.add(fileURI);
+            }
+
+            emailIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, urls);
+            emailIntent.addFlags(FLAG_GRANT_READ_URI_PERMISSION);
+            emailIntent.addFlags(FLAG_GRANT_WRITE_URI_PERMISSION);
+            startActivity(Intent.createChooser(emailIntent, "Send logs..."));
+        }
 
         measurementsList = new ArrayList<>();
         measurementsList.clear();
@@ -125,13 +151,28 @@ public class MainActivity extends AppCompatActivity {
         return resultList;
     }
 
-    private final String getCurrentTimes() {
-        long now = System.currentTimeMillis();
-        Date date = new Date(now);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        String formatDate = sdfNow.format(date);
+        if (requestCode == Utils.permissionUtils.PERMISSION_REQUEST_CODE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "위치 서비스 제한으로 앱을 수행할 수 없습니다.", Toast.LENGTH_SHORT).show();
+                btnMeasureStart.setEnabled(false);
+            } else {
+                btnMeasureStart.setEnabled(true);
 
-        return formatDate;
+                LocationManager mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+                try {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        hardwareName.setText(mLocationManager.getGnssHardwareModelName());
+                        hardwareYear.setText(String.valueOf(mLocationManager.getGnssYearOfHardware()));
+                    }
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
